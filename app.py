692557ed8ce887e2ac5891c5405f10fc416b6a8d
Istaisa <32853927+Istaisa@users.noreply.github.com>
2018-03-19 13:11:30 +0000

    Added Files = Simple App
    - HTML contains "story"
    - CSS + Background image (not done a lot with this so far...)
    - Python app, generates the random bits.

--- app.py
+++ app.py
@@ -1,22 +1,99 @@
 import os
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash, redirect, session, abort
+from random import randint
 
-app = Flask("my_first_app")
+app = Flask("story_time")
 
 @app.route("/")
-def say_hello():
-    return render_template("index.html")
+def storytime():
+    #fill in the blanks
+    character = ["human", "dwarf", "ogre", "elf", "gnome", "centaur"]
+    randomCharacter = randint(0,len(character)-1)
+    character = character[randomCharacter]
 
-@app.route("/<name>")
-def say_hello_to(name):
-    return render_template("hello.html", user=name)
+    name = ["Olga", "Arabella", "Morgana", "Beatriz", "Luna", "Hazel"]
+    randomName = randint(0,len(name)-1)
+    name = name[randomName]
 
-@app.route("/feedback", methods=["POST"])
-def get_feedback():
-    data = request.values
+    location = ["cabin in the woods", "luxury mansion", "run-down castle",
+    "spooky cave", "jungle treehouse", "thatched cottage"]
+    randomLocation = randint(0,len(location)-1)
+    location = location[randomLocation]
 
-    return render_template("feedback.html", form_data=data)
+    dream = ["owning a fishing business",
+    "becoming the greatest chef in the land",
+    "sleeping for sixteen hours a day and not much else",
+    "creating powerful spells and potions",
+    "opening a rescue centre for abandoned magical creatures",
+    "destroying everyone and everything in their path"]
+    randomDream = randint(0,len(dream)-1)
+    dream = dream[randomDream]
+
+    destiny = ["avenge their parents",
+    "become a grandmaster chess player",
+    "achieve nothing spectacular",
+    "open an enormous dairy farm",
+    "become the palace cake decorator",
+    "destroy the world"]
+    randomDestiny = randint(0,len(destiny)-1)
+    destiny = destiny[randomDestiny]
+
+    activity = ["chopping wood",
+    "weaving a blanket",
+    "chatting to the local frogs",
+    "cooking a chicken",
+    "trying to turn water into wine",
+    "getting really drunk"]
+    randomActivity = randint(0,len(activity)-1)
+    activity = activity[randomActivity]
+
+    artifact = ["an ancient tome",
+    "a jewelled sceptre",
+    "a novelty colour-change mug",
+    "a toy snake",
+    "a magical sword",
+    "a rusted mace"]
+    randomArtifact = randint(0,len(artifact)-1)
+    artifact = artifact[randomArtifact]
+
+    nemesis = ["Randolph the wizard",
+    "Chef Thompson: the second best chef in the land",
+    "Elpheba, her secret twin sister",
+    "Darius, the man who could turn into a raven",
+    "Boris the dragon",
+    "Sissy, seemingly a normal teenage girl"]
+    randomNemesis = randint(0,len(nemesis)-1)
+    nemesis = nemesis[randomNemesis]
+
+    landscape = ["scorching deserts",
+    "an enchanted forest",
+    "a fairly small town",
+    "misty mountains",
+    "an intricate forgotten mine",
+    "landscapes you could only dream of"]
+    randomLandscape = randint(0,len(landscape)-1)
+    landscape = landscape[randomLandscape]
+
+    companion = ["Pepe the frog",
+    "Reginald the bee",
+    "Aberforth the chameleon",
+    "Fifi the miniature horse",
+    "Alfonso the siberian tiger",
+    "Flappy the enormous butterfly"]
+    randomCompanion = randint(0,len(companion)-1)
+    companion = companion[randomCompanion]
+
+    task = ["return",
+    "destroy",
+    "paint a portrait of",
+    "create a display cabinet for",
+    "stare at",
+    "work out the secrets of"]
+    randomTask = randint(0,len(task)-1)
+    task = task[randomTask]
+
+    return render_template("index.html", **locals())
 
 """
 This piece of logic checks whether you are running the app locally or on Heroku
